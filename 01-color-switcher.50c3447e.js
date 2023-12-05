3b2a5e6878f9a23e63c966d6667d9b07b976c179
mlynarska-paulina <126347882+mlynarska-paulina@users.noreply.github.com>
2023-12-05 19:53:30 +0000

    Deploying to gh-pages from @ mlynarska-paulina/goit-js-hw-09@76a3d7f46b661097224cb04567136fb70b09d4ec 🚀

---
+++ 01-color-switcher.50c3447e.js
@@ -0,0 +1,2 @@
+let e=null;document.querySelector("[data-start]").addEventListener("click",(()=>{e=setInterval((()=>{const e=`#${Math.floor(16777215*Math.random()).toString(16)}`;document.body.style.background=e}),1e3),document.querySelector("[data-start]").disabled=!0,document.querySelector("[daya-stop]").disabled=!1})),document.querySelector("[data-stop]").addEventListener("click",(()=>{clearInterval(e),document.querySelector("[data-start]").disabled=!1,document.querySelector("[data-stop]").disabled=!0}));
+//# sourceMappingURL=01-color-switcher.50c3447e.js.map
