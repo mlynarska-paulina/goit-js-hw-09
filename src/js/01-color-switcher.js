fbe28f31aae555a1b99f7e216d558431937cd923
Paulina Mlynarska <paulinamlynarska@outlook.com>
2023-12-05 19:57:44 +0000

    changes

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,19 +2,19 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-let intervalId = null;
+let interval = null;
 
 document.querySelector('[data-start]').addEventListener('click', () => {
-  intervalId = setInterval(() => {
+  interval = setInterval(() => {
     const randomColor = getRandomHexColor();
-    document.body.style.background = randomColor;
+    document.body.style.backgroundColor = randomColor;
   }, 1000);
   document.querySelector('[data-start]').disabled = true;
   document.querySelector('[daya-stop]').disabled = false;
 });
 
 document.querySelector('[data-stop]').addEventListener('click', () => {
-  clearInterval(intervalId);
+  clearInterval(interval);
   document.querySelector('[data-start]').disabled = false;
   document.querySelector('[data-stop]').disabled = true;
 });
